e556c2779104b153569707ed77bc2e599d59e39d
Kirsten <kh_2015@yahoo.com>
2023-02-24 23:48:11 +0000

    a few funcs and vars for the logic

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,34 +4,34 @@ var questionSet =
   {
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: 2
+    answer: "alerts"
   },
   
   {
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: 2
+    answer: "parentheses"
   }, 
 
 
   {
     title: "Arrays in Javascript can be used to store",
     choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    answer: 3
+    answer: "all of the above"
 
   },
 
   {
     title: "Strings within Javascript must be enclosed within ____ when being assigned to variables",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: 2
+    answer: "quotes"
 
   },
 
   {
     title: "A useful tool used during development and de-bugging to print content to the de-bugger is: printing",
     choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    answer: 3
+    answer: "console.log"
 
   }
   
